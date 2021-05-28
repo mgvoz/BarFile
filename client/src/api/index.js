b1110d89cf0a52d1ad3011675f9fe6769388c347
Marissa <marissagvozdenovich@gmail.com>
2021-05-28 15:22:05 -0400

    first backend commit to heroku, improvements in api lookup

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://barfile.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://barfile.herokuapp.com' });
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem('profile')) {
 		req.headers.Authorization = `Bearer ${
